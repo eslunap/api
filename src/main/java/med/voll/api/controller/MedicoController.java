a18dd8060c3f47a14d106945a1715ede81fee71d
eslunap <estefany.luna@gmail.com>
2023-06-06 22:04:27 -0500

    Update MedicoController.java

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,7 @@
 package med.voll.api.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
    @PostMapping
-   public void registrarMedico() {
+   public void registrarMedico(@RequestBody String parametro) {
       System.out.println("Request recibido exitosamente");
+      System.out.println(parametro);
    }
 }
