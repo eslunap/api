7233e857d79390aa9b5f1139f6144865b25ff165
eslunap <estefany.luna@gmail.com>
2023-06-06 23:08:15 -0500

    Update Direccion.java
    creación constructor

--- src/main/java/med/voll/api/direccion/Direccion.java
+++ src/main/java/med/voll/api/direccion/Direccion.java
@@ -11,8 +11,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Direccion {
     private String calle;
-    private Integer numero;
+    private String numero;
     private String complemento;
     private String distrito;
     private String ciudad;
+
+    public Direccion(DatosDireccion direccion) {
+        this.calle = direccion.calle();
+        this.numero = direccion.numero();
+        this.distrito = direccion.distrito();
+        this.complemento = direccion.complemento();
+        this.ciudad = direccion.ciudad();
+    }
 }
