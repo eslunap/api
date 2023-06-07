19b93180603a4b70a7bdb368fec375d20466343e
eslunap <estefany.luna@gmail.com>
2023-06-07 18:33:22 -0500

    Update MedicoController.java
    se crea método UPDATE de datos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,10 +1,8 @@
 package med.voll.api.controller;
 
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.medico.DatosListadoMedico;
-import med.voll.api.medico.DatosRegistroMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -31,5 +29,12 @@ public class MedicoController {
    @GetMapping
    public Page<DatosListadoMedico> listadoMedicos(@PageableDefault(page = 0, size = 10, sort = {"nombre"}) Pageable paginacion){
       return medicoRepository.findAll(paginacion).map(DatosListadoMedico::new);
+   }
+
+   @PutMapping
+   @Transactional
+   public void actualizarMedico(@RequestBody @Valid DatosActualizarMedico datosActualizarMedico){
+      Medico medico = medicoRepository.getReferenceById(datosActualizarMedico.id());
+      medico.actualizarDatos(datosActualizarMedico);
    }
 }
