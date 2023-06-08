34a4f47809cb17f4cb0e1fad9e0b6a6d5efc967b
eslunap <estefany.luna@gmail.com>
2023-06-07 23:14:17 -0500

    Update DatosRegistroPaciente.java
    se agregan anotaciones para creación de tabla

--- src/main/java/med/voll/api/paciente/DatosRegistroPaciente.java
+++ src/main/java/med/voll/api/paciente/DatosRegistroPaciente.java
@@ -1,12 +1,18 @@
 package med.voll.api.paciente;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import med.voll.api.direccion.DatosDireccion;
 
 public record DatosRegistroPaciente(
-        String nombre,
-        String email,
-        String telefono,
+        @NotBlank String nombre,
+        @NotBlank @Email String email,
+        @NotBlank String telefono,
+        @NotBlank
+        //@Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}")
         String documentoIdentidad,
-        DatosDireccion direccion
+        @NotNull @Valid DatosDireccion direccion
 ) {
 }
