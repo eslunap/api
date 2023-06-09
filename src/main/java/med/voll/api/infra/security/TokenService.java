3d1252a4d6f4b0e7a02d431e165edfb8294ad07c
eslunap <estefany.luna@gmail.com>
2023-06-08 23:41:12 -0500

    Update TokenService.java
    validando firma

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -1,8 +1,11 @@
 package med.voll.api.infra.security;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import med.voll.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -28,6 +31,25 @@ public class TokenService {
         }catch (JWTCreationException exception){
             throw new RuntimeException();
         }
+    }
+
+    public String getSubject(String token ){
+        DecodedJWT verifier = null;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret); //validando firma
+            verifier = JWT.require(algorithm)
+                    .withIssuer("voll med")
+                    .build()
+                    .verify(token);
+            verifier.getSubject();
+        }catch (JWTVerificationException exception){
+            System.out.println(exception.toString());
+        }
+        //assert verifier !=null;
+        if (verifier.getSubject()==null){
+            throw new RuntimeException("Verifier inválido");
+        }
+        return verifier.getSubject();
     }
 
     private Instant generarFechaExpiracion(){
