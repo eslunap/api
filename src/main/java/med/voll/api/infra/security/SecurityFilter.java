5015fd8ca1532b38a7e10139508f69ba39f86802
eslunap <estefany.luna@gmail.com>
2023-06-09 00:11:14 -0500

    Update SecurityFilter.java

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -4,7 +4,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.domain.usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,19 +19,28 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     private TokenService tokenService;
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         System.out.println("El filtro está siendo llamado");
         //Obtener token de los headers
-        var token = request.getHeader("Authorization");//.replace("Bearer ","");
-/*        if (token == "" || token == null){
-            throw new RuntimeException("El token enviado no es válido");
-        }*/
-        if (token !=null) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader !=null) {
             System.out.println("Validamos que token no es null");
-            token = token.replace("Bearer ", "");
+            var token = authHeader.replace("Bearer ", "");
             System.out.println(token);
             System.out.println("tokenService: " + tokenService.getSubject(token)); //Este usuario tiene sesión?
+            var subject = tokenService.getSubject(token);
+            if (subject!=null){
+                //token válido
+                var usuario = usuarioRepository.findByLogin(subject); //retorna usuario
+                var authentication = new UsernamePasswordAuthenticationToken(usuario, null,
+                        usuario.getAuthorities()); //Forzamos un inicio de sesión
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            }
         }
         filterChain.doFilter(request, response);
     }
